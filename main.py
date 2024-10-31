de4b7881d1e97fb3ea081e909721088ccd51a62b
brubdub <brebdab1@gmail.com>
2024-10-31 04:07:14 -0500

    updated main.py with the get_num_chars function

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-def main():
-    with open('books/frankenstein.txt') as f:
-        file_contents = f.read()
-    
-    total_words = len(file_contents.split())
+from collections import Counter
 
-    print(total_words)
+def main():
+    book_path = 'books/frankenstein.txt'
+    text = get_book(book_path)
+    total_words = get_num_words(text)
+    char_counts = get_num_chars(text)
+
+    print(f'{total_words} words were found in the document.')
+    print('Here are the counts for all the characters:')
+    for char, count in char_counts.items():
+        print(f'{char} : {count}')
+
+def get_num_words(text):
+    words = text.split()
+
+    return len(words)
+
+def get_num_chars(text):
+    chars = sorted(list(text.lower()))
+    char_counts = Counter(chars)
+    
+    return char_counts
+
+def get_book(path):
+    with open(path) as f:
+        return f.read()
 
 main()
